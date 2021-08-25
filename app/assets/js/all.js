e0ff99bad8381aa059e5d32d883c671a2d577766
Joy Cheng <joy.cheng97324@gmail.com>
2021-08-25 16:00:00 +0800

    [add] space page + swiper effect

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -11,3 +11,26 @@ $(function() {
   },
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
+
+//swiper
+var swiper = new Swiper(".swiper-js", {
+  spaceBetween: 30,
+  centeredSlides: false,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4.5,
+
+    },
+  },
+
+});
