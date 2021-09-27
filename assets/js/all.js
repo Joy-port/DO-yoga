ddba52cd6829c688e07f3d9ba2f42b037572ce6f
Joy Cheng <joy.cheng97324@gmail.com>
2021-09-27 16:16:33 +0800

    Update 2021-09-27T08:16:27.216Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -37,11 +37,43 @@
 
 $(window).on("load", function () {
   $(".loader").remove();
-}); //mousescroll ->變成opacity .6 add class opacity-7
-//swiper
-//only auto play
+}); //Aos
 
-var swiperAutoPlay = new Swiper(".swiper-auto-play", {
+AOS.init({
+  // Global settings:
+  disable: false,
+  // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded',
+  // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init',
+  // class applied after initialization
+  animatedClassName: 'aos-animate',
+  // class applied on animation
+  useClassNames: false,
+  // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false,
+  // disables automatic mutations' detections (advanced)
+  debounceDelay: 50,
+  // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99,
+  // the delay on throttle used while scrolling the page (advanced)
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
+  duration: 400,
+  // values from 0 to 3000, with step 50ms
+  easing: 'ease',
+  // default easing for AOS animations
+  once: false,
+  // whether animation should happen only once - while scrolling down
+  mirror: false,
+  // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+});
+var swiper = new Swiper(".swiper-auto-play", {
   spaceBetween: 30,
   slidesPerView: 4.5,
   //一次顯示數量
@@ -74,39 +106,43 @@ var swiperAutoPlay = new Swiper(".swiper-auto-play", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   }
-}); //change + per column + row per group  //need to replace cdn to
-//<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.6/css/swiper.min.css">
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.6/js/swiper.min.js"></script>
-//swiper for index.html
+}); //swiper for recommend section
 
-var swiperPerColumn = new Swiper(".swiper-per-column", {
-  slidesPerView: 2.5,
-  //一次顯示數量
-  slidesPerGroup: 1,
-  //滑動數量
-  slidesPerColumn: 2,
-  //一欄顯示多少行內容
+var recommedSwiper = new Swiper(".swiper-per-column", {
   spaceBetween: 30,
-  slidesPerColumnFill: 'row',
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false
-  },
-  breakpoints: {
-    992: {
-      slidesPerGroup: 1,
-      slidesPerView: 2,
-      slidesPerColumn: 2
-    },
-    768: {
-      slidesPerGroup: 3,
-      slidesPerView: 1,
-      slidesPerColumn: 3
-    }
+  slidesPerView: 1,
+  grid: {
+    rows: 3,
+    fill: 'row'
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev"
+  },
+  breakpoints: {
+    576: {
+      spaceBetween: 15,
+      slidesPerView: 1.5,
+      grid: {
+        rows: 2,
+        fill: 'row'
+      }
+    },
+    768: {
+      spaceBetween: 30,
+      slidesPerView: 2.2,
+      grid: {
+        rows: 2,
+        fill: 'row'
+      }
+    },
+    992: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+        fill: 'row'
+      }
+    }
   }
 }); //QQ 超怪的 如果不改變視窗大小，會跑版，但是改了之後又沒事了
 
